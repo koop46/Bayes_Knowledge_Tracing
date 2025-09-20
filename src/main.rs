ff3babfe4416d8e361ee4cd622fdbaf67aedaf62
koop46 <ale.mar.vil46@gmail.com>
2025-09-20 12:36:03 +0200

    Done. Will return new p_known value instead of updating struct value

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     let client = BayesianKnowledgeTracer::new(0.1, 0.4, 0.1, 0.25);
     let test_value = client.update(1);
 
-    println!("{}", test_value);
+    println!("{}", client.predict_correct());
 
 }
 
@@ -15,6 +15,8 @@ struct BayesianKnowledgeTracer{
     p_learning: f64,
     p_correct_guess: f64,
     p_slip: f64,
+    p_new_known: f64,
+    
 
     }
 
@@ -26,37 +28,42 @@ impl BayesianKnowledgeTracer {
             p_known,
             p_learning,
             p_correct_guess,
-            p_slip
+            p_slip,
+            p_new_known: p_known,
+
         }
 
     }
 
-    fn test(&self) -> f64 {
-        return self.p_known
-    }
 
 
     fn update(&self, response: i32) -> f64 {
 
-        let float_response = response as f64;
 
         let p_correct_given_known = 1.0 - self.p_slip;
         let p_correct_given_unknown = self.p_correct_guess;
-        let p_correct = self.p_known * self.p_correct_given_known + (1 - self.p_known) * p_correct_given_unknown;
+        let p_correct = self.p_known * p_correct_given_known + (1.0 - self.p_known) * p_correct_given_unknown;
+        let p_known_given_response: f64;
 
-        if float_response == 1[
-            let p_known_given_response = (self.p_known * p_correct_given_known) / p_correct;
-        ]
+        if response == 1{
+            p_known_given_response = (self.p_known * p_correct_given_known) / p_correct;
+        }
         else {
             let p_incorrect_given_known = self.p_slip;
-            let p_incorrect_given_unknown = 1 - self.p_correct_guess;
-            let p_incorrect = self.p_known * self.p_incorrect_given_known + (1 - self.p_known) * p_incorrect_given_unknown;
-            let p_known_given_response = (self.p_known * p_incorrect_given_known) / p_incorrect;
+            let p_incorrect_given_unknown = 1.0 - self.p_correct_guess;
+            let p_incorrect = self.p_known * p_incorrect_given_known + (1.0 - self.p_known) * p_incorrect_given_unknown;
+            p_known_given_response = (self.p_known * p_incorrect_given_known) / p_incorrect;
         }
 
-        let p_new_known = p_known_given_response + (1 - p_known_given_response) * self.p_learning;
+        let p_new_known = p_known_given_response + (1.0 - p_known_given_response) * self.p_learning;
 
         return p_new_known
+
+    }
+
+    fn predict_correct(&self) -> f64{
+
+        return self.p_new_known * (1.0 - self.p_slip) + (1.0 - self.p_new_known) * self.p_correct_guess
 
     }
 
